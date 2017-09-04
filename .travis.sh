08c7f57b3299c1e85ccbc41b7e8f207fee0766d8
oglop <oglops@gmail.com>
2017-09-04 12:23:48 -0700

    test show all output

--- .travis.sh
+++ .travis.sh
@@ -238,7 +238,7 @@ build_tomato()
     elif [ "$TT_BUILD" == "hg32064k" ]; then
         make V1=RT-N5x-CN- V2=-140-hg320  $TT_BUILD &
     else
-        make V1=RT-N5x-CN- V2=-140 $TT_BUILD &
+        make V1=RT-N5x-CN- V2=-140 $TT_BUILD
 
     fi
 
